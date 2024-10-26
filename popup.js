5bf447ccd1dc55886e45165f6488462c5bdc6d4e
ofek cohen <ofekcohen3300@gmail.com>
2024-10-26 16:49:57 +0300

    toggle the display of the feed button and the food-options

--- popup.js
+++ popup.js
@@ -1,17 +1,27 @@
 const petImage = document.getElementById('pet');
 const feedButton = document.getElementById('feed-button');
 const foodOptions = document.getElementById('food-options');
+const foodItems = document.getElementsByClassName('food-item');
 
-// // Hover alert for petting
-// petImage.addEventListener('mouseover', () => {
-//   alert('Purr');
-// });
+// Ensure foodOptions is hidden by default
+foodOptions.style.display = "none";
 
 // Show or hide food options when the "Feed" button is clicked
 feedButton.addEventListener('click', () => {
     if (foodOptions.style.display === "none") {
         foodOptions.style.display = "block";
+        feedButton.style.display = "none";
     } else {
         foodOptions.style.display = "none";
+        feedButton.style.display = "block";
     }
 });
+
+// Add click event listeners to each food item
+for (let i = 0; i < foodItems.length; i++) {
+    foodItems[i].addEventListener('click', () => {
+        alert('Tasty!');
+        foodOptions.style.display = "none";
+        feedButton.style.display = "block";
+    });
+}
