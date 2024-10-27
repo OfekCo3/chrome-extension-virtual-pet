157a69fb828991874fb604f281d25ea675a00fe0
ofek cohen <ofekcohen3300@gmail.com>
2024-10-27 11:50:14 +0200

    add pets menu and display different pet

--- popup.js
+++ popup.js
@@ -4,8 +4,16 @@ const changePetButton = document.getElementById('change-pet-button');
 const foodOptions = document.getElementById('food-options');
 const foodItems = document.getElementsByClassName('food-item');
 const speechBubble = document.getElementById('speech-bubble');
+const petImage = document.getElementById('pet');
+const petSelection = document.getElementById('pet-selection');
+const petButtons = document.getElementsByClassName('pet-button');
+
+// const pets = ['cat', 'dog', 'hamster', 'turtle', 'parrot', 'fish'];
+const pets = ['cat', 'dog', 'hamster', 'parrot', 'fish'];
+let currentPetIndex = 0;
 
 foodOptions.style.display = "none";
+petSelection.style.display = "none";
 speechBubble.innerText = 'Mouse hover to pet!';  
 
 
@@ -26,15 +34,46 @@ feedButton.addEventListener('click', () => {
 
 // Add click event listeners to each food item
 for (let i = 0; i < foodItems.length; i++) {
-    foodItems[i].addEventListener('click', () => {
-    speechBubble.innerText = 'Tasty!';
-    foodOptions.style.display = "none";
-    feedButton.style.display = "block";
-    changePetButton.style.display = "block";
+    const foodItem = foodItems[i];
 
-    setTimeout(() => {
-        speechBubble.innerText = 'Mouse hover to pet!';  
-    }, 1500);
+    foodItem.addEventListener('click', () => {
+        if (foodItem.id === 'Broccoli') {
+            speechBubble.innerText = 'Ew!';
+        } else {
+            speechBubble.innerText = 'Tasty!';
+        }
 
+        foodOptions.style.display = "none";
+        feedButton.style.display = "block";
+        changePetButton.style.display = "block";
+
+        setTimeout(() => {
+            speechBubble.innerText = 'Mouse hover to pet!';
+        }, 1500);
+    });
+}
+
+// Toggle pet selection menu when "Change Pet" button is clicked
+changePetButton.addEventListener('click', () => {
+    feedButton.style.display = "none";
+    changePetButton.style.display = "none";
+    petSelection.style.display = petSelection.style.display === "none" ? "flex" : "none";
+});
+
+// Add event listeners for each pet button
+for (let i = 0; i < petButtons.length; i++) {
+    petButtons[i].addEventListener('click', (event) => {
+        const selectedPet = event.target.getAttribute('data-pet');
+        petImage.src = `images/pets/${selectedPet}.png`;
+        speechBubble.innerText = `Meet your new pet, ${selectedPet}!`;
+
+        // Hide the pet selection menu and display back feed and change pet buttons
+        petSelection.style.display = "none";
+        feedButton.style.display = "block";
+        changePetButton.style.display = "block";
+
+        setTimeout(() => {
+            speechBubble.innerText = 'Mouse hover to pet!';
+        }, 1500);
     });
 }
